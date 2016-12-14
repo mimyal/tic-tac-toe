aebf6175916de333032c10a9129db926cd3ad2c7
Alma Maizels <aymaiz@yahoo.com>
2016-12-14 10:41:03 -0800

    Board constructor tests in place

--- spec/board.spec.js
+++ spec/board.spec.js
@@ -5,8 +5,26 @@ describe('TicTacToe', function() {
   var testBoard = new Board();
   describe('Board', function() {
 
-    it('should contain a 2D array', function() {
-      expect(testBoard.spaces).toContain([]);
+    it('should set the 2D array correctly', function() {
+      expect(
+        testBoard.spaces[0][0]== 1 &&
+        testBoard.spaces[0][1]== 2 &&
+        testBoard.spaces[0][2]== 3
+      ).toBeTruthy();
+      expect(
+        testBoard.spaces[1][0]== 4 &&
+        testBoard.spaces[1][1]== 5 &&
+        testBoard.spaces[1][2]== 6
+      ).toBeTruthy();
+      expect(
+        testBoard.spaces[2][0]== 7 &&
+        testBoard.spaces[2][1]== 8 &&
+        testBoard.spaces[2][2]== 9
+      ).toBeTruthy();
+    });
+    it('should set marker variables X and O', function() {
+      expect(testBoard.markerX).toEqual('X');
+      expect(testBoard.markerO).toEqual('O');
     });
   });
 });
--- src/board.js
+++ src/board.js
@@ -2,14 +2,14 @@ import Player from 'player';
 // 2D array equivalent of the tic tac toe board
 // Contents of this page: logic for the board itself
 // needs to map the html portion of board to each button with handlers to trigger events to the player & game
-var x = "X";
-var o = "O";
 
 var Board = function(){
+  this.markerX = "X";
+  this.markerO = "O";
   this.spaces = [
     [1, 2, 3],
     [4, 5, 6],
-    [6, 7, 8]
+    [7, 8, 9]
   ];
 };
 
