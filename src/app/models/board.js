94f64472df36e190ecd3f6e7fb168761c4d1fa04
Alma Maizels <aymaiz@yahoo.com>
2016-12-18 01:08:17 -0800

    game backbone logic setup, test not working

--- src/app/models/board.js
+++ src/app/models/board.js
@@ -6,79 +6,57 @@ var Board = Backbone.Model.extend({
       ["_", "_", "_"],
       ["_", "_", "_"],
       ["_", "_", "_"]
-    ] ,
-    binaryBoardRepresentation: 0b000000000000000000 // 00 is no change, 10 is player 1, 01 is player 2, 11 is invalid
+    ]
 
   },
   initialize: function() { // it can do more complex things we wont see today/this week
     // console.log("Created new contact with name " + this.get('name'));
-    var winnningBoards = {
-      0: [ // where [0,0] is the currentPosition
-        0b010101, // [0,0], [0,1], [0,2] on the currentGrid // top row
-        0b101010, // player 2
-        0b000001000001000001, // [0,0], [1,0], [2,0] // top left down
-        0b000010000010000010,
-        0b010000000100000001, // [0,0], [1,1], [2,2] // diagonal
-        0b100000001000000010
+    var winningBoards = {
+      0: [
+        [[0,0], [0,1], [0,2]], // top row
+        [[0,0], [1,1], [2,2]], // diagonal1
+        [[0,0], [1,0], [2,0]] // left down
       ],
       1: [
-        0b010101, // from 0
-        0b101010,
-        0b000100000100000100, // [0,1], [1,1], [2,1] // center down
-        0b001000001000001000
+        [[0,0], [0,1], [0,2]], // top row
+        [[0,1], [1,1], [2,1]] //center down
       ],
       2: [
-        0b010101, // from 0
-        0b101010,
-        0b010000010000010000, // [0,2], [1,2], [2,2] // top right down
-        0b100000100000100000,
-        0b000001000100010000, // [0,2], [1,1], [2,0] diagonal
-        0b000010001000100000
+        [[0,0], [0,1], [0,2]], // top row
+        [[0,2], [1,2], [2,2]], // right down
+        [[0,2], [1,1], [2,0]] // diagonal2
       ],
       3: [
-        0b000001000001000001, // from 0
-        0b000010000010000010,
-        0b000000010101000000, // [1,0], [1,1], [1,2] // second row
-        0b000000101010000000
+        [[0,0], [1,0], [2,0]], // left down
+        [[1,0], [1,1], [1,2]] // middle row
       ],
-      4: [ //center tile
-        0b010000000100000001, // [0,0], [1,1], [2,2] // diagonal
-        0b100000001000000010,
-        0b000001000100010000, // [0,2], [1,1], [2,0] diagonal
-        0b000010001000100000,
-        0b000100000100000100, // from above // center down
-        0b001000001000001000,
-        0b000000010101000000, // from above // second row
-        0b000000101010000000
+      4: [ // center tile [1,1]
+        [[0,1], [1,1], [2,1]], //center down
+        [[1,0], [1,1], [1,2]], // middle row
+        [[0,0], [1,1], [2,2]], // diagonal1
+        [[0,2], [1,1], [2,0]] // diagonal2
       ],
-      5: [ // [1,2]
-        0b010000010000010000, // from above // top right down
-        0b100000100000100000,
-        0b000000010101000000, // from above // second row
-        0b000000101010000000
+      5: [
+        [[1,0], [1,1], [1,2]], // middle row
+        [[0,2], [1,2], [2,2]], // right down
       ],
-      6: [ // [2,0]
-        0b000001000001000001, // from above // top left down
-        0b000010000010000010,
-        0b010101000000000000, // [2,0], [2,1], [2,2] third row
-        0b101010000000000000
+      6: [
+        [[2,0], [2,1], [2,2]], // bottom row
+        [[0,0], [1,0], [2,0]], // left down
+        [[0,2], [1,1], [2,0]] // diagonal2
       ],
       7: [
-        0b000100000100000100, // from above // center down
-        0b001000001000001000,
-        0b010101000000000000, // from above // third row
-        0b101010000000000000
+        [[2,0], [2,1], [2,2]], // bottom row
+        [[0,1], [1,1], [2,1]] //center down
       ],
       8: [
-        0b010000010000010000, // from above // top right down
-        0b100000100000100000,
-        0b010101000000000000, // from above // third row
-        0b101010000000000000
+        [[2,0], [2,1], [2,2]], // bottom row
+        [[0,2], [1,2], [2,2]], // right down
+        [[0,0], [1,1], [2,2]], // diagonal1
       ]
-};
 
-
-}
+    }; //end of winningBoards
+  }
 
 });
 
--- src/app/views/player_view.js
+++ src/app/views/player_view.js
@@ -16,39 +16,5 @@ var PlayerView = Backbone.View.extend({
   //render and events functions - other logic?
 });
 
-
-// var PlayerView = function(){
-  //every play that has happened deducted here (is this necessary?)
-  // this.name = "";
-  // this.mark = "";  // x for playerX or o for playerO
-  // this.playerOplays = []; // track plays
-  // this.score = 0; // winning?
-  // this.turns = 0; // how many times have you played
-  // this.turn = false; //is it your turn or the other players
-//   if(this.turns > 2){
-//      if (this.plays.includes [[0][0]], [[0][1]], [[0][2]]){
-//      winStatus = true;
-//        if (this.plays.includes [[1][0]]){
-//        }
-//      }
-//    }
-// };
-//
-//
-// //this could be useful for an automated player later
-// Player.prototype.shuffle = function(){
-//     for (let i = this.possiblePlays.length; i; i--){
-//         let j = Math.floor(Math.random() * i);
-//         [this.plays[i - 1], this.possiblePlays[j]] = [this.possiblePlays[j], this.possiblePlays[i - 1]];
-//     }
-//     console.log(this.possiblePlays);
-//     return this.possiblePlays;
-// };
-
-
-// var playerX = new Player();
-// var playerO = new Player();
-
-
 //////////////////////
 export default PlayerView;
